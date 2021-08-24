fdc59001100b57711cdfaa230143b002462a913e
tkivite <tkivite@gmail.com>
2021-08-24 22:08:05 +0300

    update route for new registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
 end
 
