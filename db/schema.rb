aca3bbde343fbb61c4d8592f8da8cd115f4d18d6
Titus <tkivite@gmail.com>
2021-08-25 12:20:21 +0300

    merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,18 @@ ActiveRecord::Schema.define(version: 2021_08_24_191714) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "firstname", default: "", null: false
+    t.string "lastname", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
