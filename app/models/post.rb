3ba9e79fa22005936f5008c5c73786dad60aa25b
tkivite <tkivite@gmail.com>
2021-08-25 22:29:14 +0300

    added methods to load posts comments and child comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :user
 
     has_many :comments, dependent: :destroy
+
+    def count_comments
+        Comment.where(post_id: id, parent_type: 'POST').count
+    end
+
+    def comments
+        Comment.where(post_id: id, parent_type: 'POST')
+    end
 end
