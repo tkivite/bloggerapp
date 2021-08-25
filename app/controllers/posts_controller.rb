67fb4ecefe86a2d4f5b55b97c5b9167ac8e2b2eb
Titus <tkivite@gmail.com>
2021-08-25 16:51:32 +0300

    pages and loyout for adding a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
+    end
+
+    def new        
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save 
+          redirect_to post_path(@post) 
+        else
+          redirect_to root_path
+        end
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content, :title, :image)
+                             .merge(user_id: current_user.id)
       end
 end
