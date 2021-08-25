e72617994f90ea4221523357785c7079bda1d7af
tkivite <tkivite@gmail.com>
2021-08-26 01:31:31 +0300

    functionality to like and unlike a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
 
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     has_one_attached :upload_image
 
     def count_comments
