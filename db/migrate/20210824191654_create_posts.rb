09fb6a1ad9dae1f7b1a5a34d68ea12c1c4458ef7
Titus <tkivite@gmail.com>
2021-08-25 12:07:12 +0300

    created post and comment migrations

--- db/migrate/20210824191654_create_posts.rb
+++ db/migrate/20210824191654_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.text :content
+      t.string :image
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
--- db/migrate/20210824191714_create_comments.rb
+++ db/migrate/20210824191714_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.text :content
+      t.string :parent_type
+      t.integer :parent_id
+      t.belongs_to :post, index: true
 
       t.timestamps
     end
