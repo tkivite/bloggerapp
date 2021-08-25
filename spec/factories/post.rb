946f98cb076a8c83441f36a343704b95f6bde82e
Titus <tkivite@gmail.com>
2021-08-25 13:01:47 +0300

    added specs for model associations

---
+++ spec/factories/post.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+    factory :post do
+      title 'a' * 20
+      content 'a' * 20
+      image 'a' * 20
+      user      
+    end
+  end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  context 'Associations' do
+    it 'belongs_to post' do
+      association = described_class.reflect_on_association(:post).macro
+      expect(association).to eq :belongs_to
+    end
+  end
+end
